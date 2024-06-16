1b67791637982b14a4b14b9140ed9af7abc5b42f
Ejandrad <eandradel1@miumg.edu.gt>
2024-06-15 22:24:21 -0600

    a

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+
+import { AppComponent } from './app.component';
+import { TreeComponent } from './tree/tree.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { VariosComponent } from './app/varios/varios.component';
+import { InfComponent } from './inf/inf.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    TreeComponent,
+    HomeComponent,
+    ContactComponent,
+    VariosComponent,
+    InfComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
